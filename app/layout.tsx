474c040f39acfd67912d8c14530e59c4c5cc8ad4
junman140 <godswillitina@gmail.com>
2026-02-24 15:21:21 +0100

    dfg

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
             
             if (window.Pi) {
               console.log('Pi SDK loaded, initializing...');
-              window.Pi.init({ version: "2.0", sandbox: true });
+              window.Pi.init({ version: "2.0" });
               console.log('Pi SDK initialized successfully');
             } else {
               console.warn('Pi SDK not available - this app requires Pi Browser');
